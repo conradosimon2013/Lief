f0d0ebd8051d6210a33039513b6696ef0a81afa3
Conrado Joaquín Simon <conradosimon2013@gmail.com>
2023-11-29 00:42:34 -0300

    pasar parte de head modificado de la otra pc a la nube aca agregue main

---
+++ include/main.php
@@ -0,0 +1,29 @@
+
+<!--Publicacion común-->
+<main>
+    <div class="infopubli">
+        <div class="contentinfo">
+        <a href="" class="">
+            <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png" class="photoperfil" alt="">
+            <h4>Conrado Simon</h4>
+        </a>
+        <i href="" class="option fa-solid fa-ellipsis-vertical fa-fw"></i>
+        </div>
+        
+        <p class="descriptionpubli">Este aguila re webon parece pato</p>
+        
+        <div class="content">
+            
+            <img src="https://img.freepik.com/foto-gratis/pajaro-palabra-aguila-sus-alas_1340-33393.jpg?size=626&ext=jpg&ga=GA1.1.1826414947.1699056000&semt=sph" alt=""> 
+            <div> 
+             <p><i class="fa-regular fa-heart"></i>Me gusta</p> <!--fa-solid fa-heart a los demas tambien solid-->
+             <p><i class="fa-regular fa-comment"></i> Comentar</p>
+             <p><i class="fa-regular fa-share-from-square"></i>Compartir</p>
+
+            </div>
+        </div>
+
+
+
+    </div>
+</main>
--- index.php
+++ index.php
@@ -7,10 +7,12 @@
     <link rel="stylesheet" href="css/style.css?v=<?php echo(rand()); ?>" >
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js">
     <script src="js/main.js"></script>
+    <script src="https://kit.fontawesome.com/63394a4df3.js" crossorigin="anonymous"></script>
 </head>
 <body>
 <?php
-include "include/header.php"
+include "include/header.php";
+include "include/main.php";
 
 ?>
     
